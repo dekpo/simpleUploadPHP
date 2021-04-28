4935fb5c948a4703dd0760d8d9c57c8b3b8006f1
Dekpo <dekpo@me.com>
2021-04-28 09:48:10 +0200

    Added a simple switch

--- upload.php
+++ upload.php
@@ -1,13 +1,29 @@
 <?php
 $monFichier = $_FILES['fichier']['name'];
-$dossier = "./images/";
+
+switch ($_FILES['fichier']['type']){
+
+    case "image/png":
+        $dossier = "./images/png/";
+        break;
+
+    case "image/jpeg":
+        $dossier = "./images/jpg/";
+        break;
+
+    default:
+        $dossier = "./images/";
+}
+
 $nouveauFichier = $dossier . basename( $monFichier );
 
 if (move_uploaded_file($_FILES['fichier']['tmp_name'], $nouveauFichier)){
     echo "<h3>Fichier uploadé avec succès !</h3>";
-    echo "<p><img src=\"$nouveauFichier\" alt=\”$nouveauFichier\” ></p>";
+    echo "<p><a href=\"index.html\">Recommencer ?</a></p>";
+    echo "<p><img src=\"$nouveauFichier\" alt=\"$nouveauFichier\" ></p>";
+    echo "\$_FILES:<pre>";
+    print_r($_FILES);
+    echo "</pre>";
 } else {
     echo "<h3>Une erreur est survenue pendant l'upload :(";
 }
-echo "<p><a href=\"index.html\">Recommencer ?</a></p>";
-
